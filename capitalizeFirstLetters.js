2c9cdb77ac0b701bce571baefc6e676478a145db
Pol <polcahh@gmail.com>
2020-10-10 20:49:51 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,17 @@
 
 const assert = require('assert');
 
+function capitalizeFirst(input) {
+    return (input.length > 0
+        ? input.split(' ').map((word) => 
+            word.split().map((letter) => 
+                letter[0].toUpperCase() + letter.slice(1)
+            ).join(' ')
+        ).join(' ')
+        : ''
+    )
+}
+
 // Check that capitalizeFirst is a function
 assert.strictEqual(typeof capitalizeFirst, 'function');
 // Check that capitalizeFirst accepts one argument
